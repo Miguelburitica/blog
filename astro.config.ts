ad725c3ea042f6ad4d577a476e5bd4ce4820f201
Miguelburitica <miguelaburiticav@gmail.com>
2024-02-02 22:40:32 -0500

    Add astro-robots-txt package

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,7 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,6 +16,7 @@ export default defineConfig({
     }),
     react(),
     sitemap(),
+    robotsTxt(),
   ],
   markdown: {
     remarkPlugins: [
